9da09f840cb610cc624de4178a6870f29df114b3
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-05 23:00:23 +0000

    adds init function + moment start

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+
+const currentDay = moment();
+const currentTime = moment().format('HH mm A')
+
+let timeBlocks = [];
+
+function init() {
+    console.log(currentTime);
+    $('#currentDay').text(currentDay.format('dddd Do MMM YYYY'));
+    for(let i = 0; i < timeBlocks.length; i++){
+
+    }
+}
+
+init();
