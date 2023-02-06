2d78f8c1f8a3561e059f2022229e6bed6a3f894a
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-06 00:54:57 +0000

    adds button attributes and local Storage first pass

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 
 const currentDay = moment();
 const currentTime = moment().format('HH mm A')
+let buttonCount = 0;
 
 let timeBlocks = [
     time1 = {
@@ -38,11 +39,17 @@ let timeBlocks = [
     }
 ];
 
+let userEvents = [];
+
 
 function init() {
     console.log(currentTime);
 
     $('#currentDay').text(currentDay.format('dddd Do MMM YYYY'));
+
+
+
+
 
 
     for(let i = 0; i < timeBlocks.length; i++){
@@ -51,9 +58,43 @@ function init() {
         if (timeBlocks[i].time.isAfter(currentDay, 'minute')){
             console.log(timeBlocks[i].time + ' is after this time')
             $(timeBlocks[i].id).addClass('future');
+        }else if(timeBlocks.time.isBefore(currentDay, 'minute')){
+            $(timeBlocks[i].id).addClass('past')
+        }
+    }
+
+    while(buttonCount < 9){
+        $('#btn' + buttonCount).on('click', function(){
+            $('#btn' + buttonCount).data('num', buttonCount);
+            let buttonID = jQuery.data($('#btn' + buttonCount), 'num');
+            console.log(buttonID);
+            console.log('HELLO');
+            var input = $('#' + buttonID).val();
+            console.log(input);
+            let event = {
+                text: input
+            }
+            console.log(event);
+            JSON.stringify(event);
+            console.log(event);
+            userEvents.push(event);
+            saveData();
+        })
+        buttonCount++;
+    }
+    buttonCount = 0;
+
+
+        let data = localStorage.getItem('userData');
+        for(let i = 0; i < data.length; i++){
+            let input = $('#' + (i + 1));
+            input.val(data[i].text);
         }
 
-    }
+}
+
+function saveData(){
+    localStorage.setItem('userData', JSON.stringify(userEvents));
 }
 
 init();
