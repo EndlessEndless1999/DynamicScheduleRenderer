d416bccc097a667bdbede8298307e4a2b3d2705a
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-06 01:23:08 +0000

    fixes localStorage bugs

--- assets/script.js
+++ assets/script.js
@@ -4,38 +4,48 @@ const currentDay = moment();
 const currentTime = moment().format('HH mm A')
 let buttonCount = 0;
 
+let buttons = [$('#btn1'), $('#btn2'), $('#btn3'), $('#btn4'), $('#btn5'), $('#btn6'), $('#btn7'), $('#btn8')];
+
 let timeBlocks = [
     time1 = {
         time: moment('09:00', 'HH mm A'),
-        id: '#1'
+        id: '#1',
+        buttonId: '#btn1'
     },
     time2 = {
         time: moment('10:00', 'HH mm A'),
-        id: '#2'
+        id: '#2',
+        buttonId: '#btn2'
     },
     time3 = {
         time: moment('11:00', 'HH mm A'),
-        id: '#3'
+        id: '#3',
+        buttonId: '#btn3'
     },
     time4 = {
         time: moment('12:00', 'HH mm A'),
-        id: '#4'
+        id: '#4',
+        buttonId: '#btn4'
     },
     time5 = {
         time: moment('13:00', 'HH mm A'),
-        id: '#5'
+        id: '#5',
+        buttonId: '#btn5'
     },
     time6 = {
         time: moment('14:00', 'HH mm A'),
-        id: '#6'
+        id: '#6',
+        buttonId: '#btn6'
     },
     time7 = {
         time: moment('15:00', 'HH mm A'),
-        id: '#7'
+        id: '#7',
+        buttonId: '#btn7'
     },
     time8 = {
         time: moment('16:00', 'HH mm A'),
-        id: '#8'
+        id: '#8',
+        buttonId: '#btn8'
     }
 ];
 
@@ -51,7 +61,7 @@ function init() {
 
 
 
-
+    //SETTING CLASS THAT INDICATES PAST OR FUTURE
     for(let i = 0; i < timeBlocks.length; i++){
         console.log('is working');
         console.log(timeBlocks[i].time);
@@ -63,36 +73,41 @@ function init() {
         }
     }
 
-    while(buttonCount < 9){
-        $('#btn' + buttonCount).on('click', function(){
-            $('#btn' + buttonCount).data('num', buttonCount);
-            let buttonID = jQuery.data($('#btn' + buttonCount), 'num');
-            console.log(buttonID);
-            console.log('HELLO');
-            var input = $('#' + buttonID).val();
-            console.log(input);
-            let event = {
-                text: input
-            }
-            console.log(event);
-            JSON.stringify(event);
-            console.log(event);
-            userEvents.push(event);
-            saveData();
-        })
-        buttonCount++;
+    //CREATING EVENTLISTENER THAT SAVES EVENTS IN LOCAL STORAGE
+
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].attr('input', i + 1);
+        buttons[i].on('click', onClick);
     }
-    buttonCount = 0;
 
 
-        let data = localStorage.getItem('userData');
-        for(let i = 0; i < data.length; i++){
-            let input = $('#' + (i + 1));
-            input.val(data[i].text);
-        }
+
+
+
+
+
+    if(userEvents != null){for(let i = 0; i < userEvents.length; i++){
+        let input = $('#' + (i + 1));
+        input.val(data[i].text);
+    }}
+
 
 }
 
+function onClick(){
+    input = this.getAttribute('input');
+    inputField = $('#' + input).val();
+    console.log(input);
+    let event = {
+        text: inputField,
+        id: input
+    }
+    console.log(event);
+    userEvents.push(event);
+    saveData();
+}
+
+//SAVES DATA IN LOCAL STORAGE
 function saveData(){
     localStorage.setItem('userData', JSON.stringify(userEvents));
 }
